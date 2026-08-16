91675bfddfbb24d947258429167aa25d56f42608
Vijay-001-Vlsi <vijaym08m@gmail.com>
2026-08-16 20:35:02 +0530

    Update generator.sv

--- src/test_bench/generator.sv
+++ src/test_bench/generator.sv
@@ -1,9 +1,6 @@
 class generator;
         mailbox #(transaction)gd_mbx;
         transaction tx;
-
-        logic [`ADDR_WIDTH-1:0] written_addr[$];
-
         function new(mailbox #(transaction)gd_mbx);
                 this.gd_mbx=gd_mbx;
                 tx=new();
@@ -13,14 +10,6 @@ class generator;
                 for(int i=0;i<`num_transaction;i++)begin
                         assert(tx.randomize())
                         else $fatal("[GEN]randomization failed");
-
-                        if(tx.present_state==access && !tx.pwrite &&
-                           written_addr.size()>0 && ($urandom_range(0,3)==0)) begin
-                                tx.addr      = written_addr[$urandom_range(0,written_addr.size()-1)];
-                                tx.temp_addr = tx.addr;
-                        end
-                        if(tx.present_state==access && tx.pwrite && tx.addr<`MEM_DEPTH)
-                                written_addr.push_back(tx.addr);
                         tx.print("GEN");
                         gd_mbx.put(tx.copy());
                 end
