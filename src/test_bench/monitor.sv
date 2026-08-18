ee3e7a20eee16e9518863bb6bd05b0e4a50c670f
Vijay-001-Vlsi <vijaym08m@gmail.com>
2026-08-18 18:19:56 +0530

    Update monitor.sv

--- src/test_bench/monitor.sv
+++ src/test_bench/monitor.sv
@@ -17,8 +17,6 @@ class monitor;
                 $display("[MONITOR][%0t]sample PSEL=%0b PENABLE=%0b PADDR=%0d PWRITE=%b PREADY=%0b PSLVERR=%0b",
                           $time, vif.mon.PSEL, vif.mon.PENABLE, vif.mon.PADDR, vif.mon.PWRITE,
                           vif.mon.PREADY, vif.mon.PSLVERR);
-
-                if(vif.mon.PSEL && vif.mon.PENABLE) begin
                         txn=new();
                         txn.addr=vif.mon.PADDR;
                         txn.pwdata=vif.mon.PWDATA;
@@ -32,7 +30,6 @@ class monitor;
                         $display("[MONITOR][%0t]to SCOREBOARD PSEL=%0b PENABLE=%0b,PADDR=%0d,PWRITE=%b,PWDATA=%d,PSTRB=%0b PSLVERR=%0b,PREADY=%0b,PRDATA=%0d",
                                   $time,txn.sel,txn.penable,txn.addr,txn.pwrite,txn.pwdata,txn.pstrb,txn.pslverr,txn.pready,txn.prdata);
                         act_mbx.put(txn);
-                end
         end
 endtask	
 endclass
